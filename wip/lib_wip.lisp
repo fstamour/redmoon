4673a38b44d398d6c896357ba2539011b7de2644
Francis St-Amour <m_psyco2@hotmail.com>
2018-02-23 23:48:33 -0500

    Moved some untracked files into a "work in progess" folder.

---
+++ wip/lib_wip.lisp
@@ -0,0 +1,72 @@
+
+(cl:in-package :redmoon.user)
+
+(def max (x y)
+  (if (> x y) x y))
+
+(def min (x y)
+  (if (> x y) y x))
+
+(def abs (x)
+  (if (< 0 x) (- x) x))
+
+(def log2 (x)
+  (set n 0)
+  (while (> 0 x)
+    (if (pairp x)
+        (set x (/ x 2)
+             n (+ n 1))
+        (set x (- x 1)))))
+
+(def sqrt (n)
+  ;; (if (< 1 n) (return 0))
+  ;; (if (= 1 n) (return 1))
+  (set x0 (/ n 2)
+       x1 (/ (+ (/ n x0)) 2))
+  (while (> (abs (- x1 x0)) 1)
+    (set x0 x1
+         x1 (/ (+ (/ n x0)) 2)))
+  (while (> (* x1 x1) n)
+    (set x1 (- x1 1)))
+  x1)
+
+(def elegant-pair (x y)
+  (if (> x y)
+      (+ (* x x) x y)
+      (+ (* y y) x)))
+
+;; Need to return a tuple.
+(def elegant-unpair (p)
+  (set sroot (sqrt p)
+       p* (* sroot sroot)
+       x (- p p*)
+       y sroot)
+  (if (>= x y)
+      (set y (- p p* sroot)
+           x sroot))
+  #(x y))
+
+
+(def encode (x)
+  (if (< n 0) (- n 1) (+ n 1)))
+
+(def decode (x)
+  (if (< n 0) (+ n 1) (- n 1)))
+
+;; Needs 'list' and 'for'.
+(def list (int-list init)
+  (set result init)
+  (for (i int-list)
+       (set result (elegant-pair result (encode i))))
+  result)
+
+;; Needs destructuring
+(def reverse (l)
+  (set (x y) (elegant-unpair l) ;; Here
+       result 0)
+  (while (/= 0 y)
+    (set result (list y result)
+         (x y) (elegant-unpair x)))  ;; And here
+  resutl)
+
+
