5298de854f89262d46c46c81d59cec8e2d8e595e
Francis St-Amour <m_psyco2@hotmail.com>
2018-03-04 01:34:29 -0500

    Migrated a bunch of tests for checkl to parachute.

--- tests/core.lisp
+++ tests/core.lisp
@@ -3,7 +3,6 @@
 (define-test keyword?
   (false (keyword? :x))
   (false (keyword? 'x))
-
   (true (keyword? :if))
   (true (keyword? :set))
   (true (keyword? :while))
@@ -19,7 +18,6 @@
   (true (keyword? :>=))
   (true (keyword? :true))
   (true (keyword? :false))
-
   (true (keyword? 'if))
   (true (keyword? 'set))
   (true (keyword? 'while))
@@ -36,10 +34,14 @@
   (true (keyword? 'true))
   (true (keyword? 'false)))
 
-
 (define-test var?
   :depends-on (keyword?)
-  (true (var? 'x)))
+  (true (var? 'x))
+  (false (var? 'true))
+  (false (var? 'false))
+  (false (var? 'if))
+  (false (var? '(any thing)))
+  (false (var? 'while)))
 
 (define-test assignation?
   (true (assignation? '(set x 42)))
@@ -53,7 +55,10 @@
   (true (atom? 42))
   (true (atom? 42.5))
   (true (atom? 'symbol))
+  (true (atom? 'true))
+  (true (atom? 'false))
   (true (atom? :keyword))
+  (false (atom? '()))
   (false (atom? '(some list))))
 
 (define-test integer?
@@ -69,7 +74,6 @@
   (false (bool? t))
   (false (bool? nil))
   (false (bool? 42)))
-
 
 (define-test function?
   (false (function? '(set x 42)))
@@ -88,19 +92,67 @@
   (is eq :false (to-bool nil)))
 
 (define-test truep
+  (true (truep 'true))
+  (false (truep 'false))
   (true (truep :true))
   (false (truep :false))
   ;; Anything else should signal an error.
   (fail (truep 42)))
 
 (define-test eval-atom
-  (is eq 42 (eval-atom 42))
+  (is = 42 (eval-atom 42))
   (fail (eval-atom 42.5))
   (is eq :true (eval-atom :true))
   (is eq :false (eval-atom :false))
   (fail (eval-atom 'x))
-  (is 'eq 42
+  (is = 42 (eval-atom 'x (make-env '(x 42))))
+  (is = 42
       (with-env
         (eval-set '(set x 42) *top-level-environment*)
         (eval-atom 'x *top-level-environment*))))
+
+(define-test eval/atom
+  (is = 1 (eval 1))
+  (is eq :false (eval :false))
+  (is = 42 (eval 'x (make-env '(x 42)))))
+
+(define-test eval/sequence
+  (is = 4 (eval '(2 3 4)))
+  (is = 3 (eval '((+ 1 2))))
+  (is = 4 (eval '((+ x 2)) (make-env '(x 2)))))
+
+(define-test not
+  (is eq :false (eval-not '(not :true) (make-env)))
+  (is eq :true (eval-not '(not :false) (make-env)))
+  (is eq :true (eval-not '(not x) (make-env '(x :false)))))
+
+(defun truth-table (op n)
+  (apply #'map-product
+         (lambda (&rest b) `(,op ,@b))
+         (loop :for i :below n :collect '(true false))))
+
+(define-test or
+  (is equal '(:true :true :true :false)
+      (mapcar 'eval (truth-table 'or 2))))
+
+(define-test and
+  (is equal '(:false :false :false :true)
+      (mapcar 'eval (truth-table 'and 2))))
+
+(define-test +
+  (is = 3 (eval '(+ 1 2)))
+  (is = 1 (eval '(+ x 1) (make-env '(x 0)))))
+
+(define-test -
+  (is = 0 (eval '(- 1 1)))
+  (is = -1 (eval '(- x 1) (make-env '(x 0)))))
+
+(define-test *
+  (is = 4 (eval '(* 2 2)))
+  (is = 0 (eval '(* x 1) (make-env '(x 0)))))
+
+;; integer division
+(define-test /
+  (is = 3 (eval '(/ 10 3)))
+  (is = 0 (eval '(/ x 5) (make-env '(x 2)))))
 
