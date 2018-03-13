a4f48841a64db17aa24b571974069aaf9d56158f
Francis St-Amour <fstamour@users.noreply.github.com>
2018-03-13 00:20:00 -0400

    CI - Renamed folder docker -> ci. Added shell script to run the test. (#4)
    Renamed the folder "docker" to "ci". Added a nifty little shell script to run the tests.

---
+++ ci/sbcl-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+redmoon_root=$(git rev-parse --show-toplevel)
+
+sbcl --noinform --non-interactive \
+    --eval '(push "'"$redmoon_root"'" ql:*local-project-directories*)' \
+    --eval '(ql:quickload :redmoon.test)' \
+    --eval '(asdf:test-system :redmoon)'
+
