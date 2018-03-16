98bb82faa7d8e58043097974c8073866e2a0bd3b
Francis St-Amour <m_psyco2@hotmail.com>
2018-03-16 01:31:19 -0400

    Cleaner tests for the type system

--- tests/type.lisp
+++ tests/type.lisp
@@ -9,43 +9,40 @@
        ;; (break)
        ,(append (first (last (last body)))
                 '(redmoon:*top-level-environment* *top-level-constraint*))))
+  (defmacro is-type (type &body body)
+    `(is eq ,type (with-env ,@body)))
+  (defmacro is-type* (type &body body)
+    `(is eq ,type (with-env* ,@body)))
   (defmacro check-constraint (var type &body body)
     `(is eq ,type (with-env ,@body (get-constraint ,var *top-level-constraint*)))))
 
 (define-test integer!
-  (is eq :integer
-      (with-env* (integer! 2)))
-  (false
-      (with-env*
-        (add-constraint 'x :bool *top-level-constraint*)
-        (integer! 'x)))
-  (is eq :integer
-      (with-env*
-        (integer! 'x))))
+  (is-type* :integer (integer! 2))
+  (is-type* nil
+            (add-constraint 'x :bool *top-level-constraint*)
+            (integer! 'x))
+  (is-type* :integer (integer! 'x)))
 
 (define-test bool!
-  (is eq :bool (with-env* (bool! 'true)))
-  (is eq :bool (with-env* (bool! 'false)))
-  (is eq :integer (with-env* (bool! 42))) ;; That's not confusing at all. 
-  (false
-      (with-env*
-        (add-constraint 'x :integer *top-level-constraint*)
-        (bool! 'x))))
+  (is-type* :bool (bool! 'true))
+  (is-type* :bool (bool! 'false))
+  (is-type* :integer (bool! 42)) ;; That's not confusing at all. 
+  (is-type* nil
+    (add-constraint 'x :integer *top-level-constraint*)
+    (bool! 'x)))
 
 (define-test modulo
   (check-constraint 'n :integer (typeof '(mod n 2)))
-  (false (with-env
-           (integer? '(mod n 2)))))
+  (is-type nil (integer? '(mod n 2))))
 
 
 (define-test comparison
-  (is eq :bool (with-env (typeof '(< 1 0))))
-  (is eq :bool (with-env (typeof '(> 1 0))))
-  (is eq :bool (with-env (typeof '(= 1 0))))
-  (is eq :bool (with-env (typeof '(/= 1 0))))
-  (is eq :bool (with-env (typeof '(<= 1 0))))
-  (is eq :bool (with-env (typeof '(>= 1 0))))
-
+  (is-type :bool  (typeof '(< 1 0)))
+  (is-type :bool (typeof '(> 1 0)))
+  (is-type :bool (typeof '(= 1 0)))
+  (is-type :bool (typeof '(/= 1 0)))
+  (is-type :bool (typeof '(<= 1 0)))
+  (is-type :bool (typeof '(>= 1 0))) 
   (check-constraint 'n :integer (typeof '(< n 0)))
   (check-constraint 'n :integer (typeof '(> n 0)))
   (check-constraint 'n :integer (typeof '(= n 0)))
